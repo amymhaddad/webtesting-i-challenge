9f4addf15a50edc610e6557f1fe5033ad22a79ed
Amy M Haddad <amy.m.haddad@gmail.com>
2020-10-12 07:02:35 -0400

    Completed project

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,33 @@
 module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+	success,
+	fail,
+	repair,
+	get
 };
 
+const decreaseEnhancement = { 17: 16, 18: 17 };
+
 function success(item) {
-  return { ...item };
+	if (item.enhancement < 20) return { ...item, enhancement: (item.enhancement += 1) };
+	else return item;
 }
 
 function fail(item) {
-  return { ...item };
+	if (item.enhancement < 15) return { ...item, durability: (item.durability -= 5) };
+
+	if (item.enhancement >= 15 && item.enhancement < 17) return { ...item, durability: (item.durability -= 10) };
+
+	if (item.enhancement >= 17) return { ...item, enhancement: decreaseEnhancement[item.enhancement] };
 }
 
 function repair(item) {
-  return { ...item };
+	return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+	let name = item.name;
+	let enhancement = item.enhancement;
+
+	if (enhancement === 0) return item;
+	if (enhancement > 0) return { ...item, name: `[+${enhancement}] ${name}` };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,67 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe("Update an item's output based on specific conditions", () => {
+	describe('Restore durability of an item', () => {
+		test('Update durability to 100', () => {
+			const item = { name: 'name', durability: 90, enhancement: 20 };
+			expect(enhancer.repair(item)).toHaveProperty('durability', 100);
+		});
+	});
+
+	describe('A successful enhancement increments by one unless its value is 20', () => {
+		test('Successful enhancement increments by 1 if less than 20', () => {
+			const item = { name: 'name', durability: 90, enhancement: 19 };
+			const itemWithUpdatedEnhancement = { name: 'name', durability: 90, enhancement: 20 };
+			expect(enhancer.success(item)).toEqual(itemWithUpdatedEnhancement);
+		});
+		test('Successful enhancement is not changed if equal to 20 ', () => {
+			const item = { name: 'name', durability: 90, enhancement: 20 };
+			expect(enhancer.success(item)).toEqual(item);
+		});
+	});
+
+	describe('A failed enhacement decreases the durability of the item', () => {
+		test("An item's enhancement is less than 15", () => {
+			const item = { name: 'name', durability: 90, enhancement: 14 };
+			const updatedItem = { name: 'name', durability: 85, enhancement: 14 };
+			expect(enhancer.fail(item)).toEqual(updatedItem);
+		});
+
+		test("An item's enhancement equals 15", () => {
+			const item = { name: 'name', durability: 90, enhancement: 15 };
+			const updatedItem = { name: 'name', durability: 80, enhancement: 15 };
+			expect(enhancer.fail(item)).toEqual(updatedItem);
+		});
+
+		test("An item's enhancement is greater than 15 but less than 17", () => {
+			const item = { name: 'name', durability: 90, enhancement: 16 };
+			const updatedItem = { name: 'name', durability: 80, enhancement: 16 };
+			expect(enhancer.fail(item)).toEqual(updatedItem);
+		});
+
+		test("An item's enhancement is 17", () => {
+			const item = { name: 'name', durability: 90, enhancement: 17 };
+			const updatedItem = { name: 'name', durability: 90, enhancement: 16 };
+			expect(enhancer.fail(item)).toEqual(updatedItem);
+		});
+
+		test("An item's enhancement is 18", () => {
+			const item = { name: 'name', durability: 90, enhancement: 18 };
+			const updatedItem = { name: 'name', durability: 90, enhancement: 17 };
+			expect(enhancer.fail(item)).toEqual(updatedItem);
+		});
+	});
+
+	describe("Update an item's name based on the enhacement level", () => {
+		test('Enhancement level is 0', () => {
+			const item = { name: 'name', durability: 90, enhancement: 0 };
+			expect(enhancer.get(item)).toEqual(item);
+		});
+
+		test('Enhancment level is greater than 0', () => {
+			const item = { name: 'Iron Sword', durability: 90, enhancement: 7 };
+			const updatedItem = { name: '[+7] Iron Sword', durability: 90, enhancement: 7 };
+			expect(enhancer.get(item)).toEqual(updatedItem);
+		});
+	});
+});
